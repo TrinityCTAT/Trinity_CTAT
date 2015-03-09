0371d6573175a7d870f46acbffa54e8c6664c6d9
Brian Haas <brianjohnhaas@gmail.com>
2015-03-09 11:06:35 -0400

    update, works w/ kcopipe

--- fusion/test/__runMe_kcopipe.sh
+++ fusion/test/__runMe_kcopipe.sh
@@ -12,6 +12,7 @@ reuse  Java-1.7
 reuse .coreutils-8.22
 reuse .samtools-0.1.19
 
+export PYTHONPATH=/seq/regev_genome_portal/SOFTWARE/PYTHON_LIB:${PYTHONPATH}
 export HISAT_HOME=/seq/regev_genome_portal/SOFTWARE/HISAT/current
 export TRINITY_HOME=/home/unix/bhaas/GITHUB/trinityrnaseq
 export PATH=/home/unix/bhaas/utilities:${PATH}
