e3190ea08e231dc213337130c7f74b4ca12c4215
Brian Haas <brianjohnhaas@gmail.com>
2015-03-15 20:07:27 -0400

    add

---
+++ genome_guided_transcript_reconstruction/test/cleanme.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use FindBin;
+
+
+## we delete all files we don't need in this directory. Be careful in case users try running it somewhere else, outside this dir.
+chdir $FindBin::Bin or die "error, cannot cd to $FindBin::Bin";
+
+
+
+my @files_to_keep = qw (cleanme.pl 
+                        runMe.sh
+                        );
+
+
+my %keep = map { + $_ => 1 } @files_to_keep;
+
+
+`rm -rf ./cufflinks_outdir` if (-d "cufflinks_outdir");
+
+foreach my $file (<*>) {
+	
+	if (-f $file && ! $keep{$file}) {
+		print STDERR "-removing file: $file\n";
+		unlink($file);
+	}
+}
+
+
+exit(0);
