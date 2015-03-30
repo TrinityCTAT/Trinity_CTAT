e8e1c72da27ec9c11d0c6715ee40437123a3c162
TimothyTickle <timothyltickle@gmail.com>
2015-03-30 18:44:34 -0400

    Initial add of script to combine and annotate vcf files.

---
+++ mutation/src/summarize_annotate_vcf.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+__author__ = "Timothy Tickle"
+__copyright__ = "Copyright 2015"
+__credits__ = [ "Timothy Tickle", "Brian Haas" ]
+__license__ = "MIT"
+__maintainer__ = "Timothy Tickle"
+__email__ = "ttickle@broadinstitute.org"
+__status__ = "Development"
+
+import argparse
+import os
+import sciedpiper.Command as Command
+import sciedpiper.Pipeline as Pipeline
+import sciedpiper.ParentScript as ParentScript
+
+class SummarizeAnnotateVCF( ParentScript ):
+
+  def func_update_arguments( self, args_raw ): 
+    """
+    Add in custom arguments fo rthe mutation validation pipeline.
+
+    * arg_raw : Arguments ( not yet parsed )
+              : Arguments
+    * return  : Updated Arguments
+              : Arguments
+    """
+
+    # Parse command line arguments
+    args_raw.prog = "Summarize_annotate_vcf.py"
+    args_raw.add_argument( "--dbsnp", metavar = "dbsnp_reference_vcf", dest = "str_dbsnp_vcf", default = None, required = True, action="store", help = "Reference dbsnp file to use for annotation." )
+    args_raw.add_argument( metavar = "sample_vcfs", dest = "lstr_sample_files", default = None, required = True, action="store", nargs="+",  help = "Sample files to combine vcfs." )
+    return( args_raw )
+
+
+  def func_make_commands( self, args_parsed, cur_pipeline ):
+    """
+    Make a list of command objects.
+
+    * return : List of commands to be ran
+             : List of command objects
+    """
+
+    # File of combined sample vcf files
+    str_combined_vcf = os.path.splitext( args_call.str_output_file )[0] + "_combined.vcf"
+
+    # Manage Files
+    # Make sure the output file is named gz, if not, add it
+    if not os.path.splitext( args_call.str_output_file )[ 1 ] == "gz":
+      args_call.str_output_file = args_call.str_output_file + ".gz"
+
+    # List of commands
+    lcmd_commands = []
+
+    # Combine sample vcf files
+    # bcftools merge --merge all --output_type z --output str_output_file.vcf.gz lstr_vcf_files
+    str_merge_command = ""
+    lcmd_commands.append( Command.Command( str_cur_command = str_merge_command,
+                     lstr_cur_dependencies = lstr_vcf_files,
+                     lstr_cur_products = [ str_combined_vcf ] ) )
+
+    # Annotate combined sample vcf files
+    # bcftools annotate --annotations str_dbsnp_vcf -c
+    str_annotate_command = ""
+    lcmd_commands.append( Command.Command( str_cur_command = str_annotate_command,
+                     lstr_cur_dependencies = [ str_dbsnp_vcf, str_combined_vcf ],
+                     lstr_cur_products = [ args_call.str_output_file ] ) )
+
+    # Run commands
+    return( lcmd_commands )
