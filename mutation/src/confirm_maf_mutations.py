2eef515c9d3dbc228596f11ee2a429f0772f776b
TimothyTickle <timothyltickle@gmail.com>
2015-05-19 13:25:30 -0400

    Added plotting both location and sample and outputing text.

--- mutation/src/confirm_maf_mutations.py
+++ mutation/src/confirm_maf_mutations.py
@@ -11,6 +11,7 @@ __status__ = "Development"
 import argparse
 import csv
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import os
 
 # VCF related constants
@@ -28,6 +29,14 @@ c_I_MAF_SAMPLE = 15
 c_I_MAF_REF = 10
 c_I_MAF_ALLELE_1 = 11
 c_I_MAF_ALLELE_2 = 12
+
+c_I_MAF_RETURN_POS = 0
+c_I_MAF_RETURN_GENE_NAME = 1
+c_I_MAF_RETURN_REF = 2
+c_I_MAF_RETURN_GENOTYPE = 3
+
+c_I_VCF_RETURN_REF = 0
+c_I_VCF_RETURN_GENOTYPE = 1
 
 ######## functions
 def func_read_maf( str_maf_file ):
@@ -107,6 +116,11 @@ dict_gene_confirmed = {}
 # Holds the locations that were missed
 # { gene : i_missed_instances }
 dict_gene_missed = {}
+# { gene : [ confirmed, sample,...] }
+dict_gene_confirmed_sample = {}
+# Holds the locations that were missed
+# { gene : [ missed, sample,...] }
+dict_gene_missed_sample = {}
 # Holds the locations that had the wrong ref or genotype
 # { str_pos : i_missed_ref/genotype  }
 dict_wrong_genotype_pos = {}
@@ -122,8 +136,6 @@ lstr_key_genes = None
 if args_call.str_key_mappings:
   lstr_key_genes = [ str_gene.lower() for str_gene in args_call.str_key_mappings.split(",") ]
   lstr_key_genes.reverse()
-
-###### Make sure the same genotype.
 
 # Read in maf file
 # { sample : [ pos, gene ], [ pos, gene ], ... }
@@ -146,24 +158,29 @@ for str_sample, llstr_positions in dict_maf_data.items():
     for lstr_maf_pos in llstr_positions:
       # Make sure the gene is in the key genes
       if lstr_key_genes:
-        if not lstr_maf_pos[ 1 ] in lstr_key_genes:
+        if not lstr_maf_pos[ c_I_MAF_RETURN_GENE_NAME ] in lstr_key_genes:
           continue
 
-      if lstr_maf_pos[ 0 ] in dict_vcf_snps:
-        lstr_cur_vcf_entry = dict_vcf_snps[ lstr_maf_pos[ 0 ] ]
+      if lstr_maf_pos[ c_I_MAF_RETURN_POS ] in dict_vcf_snps:
+        lstr_cur_vcf_entry = dict_vcf_snps[ lstr_maf_pos[ c_I_MAF_RETURN_POS ] ]
         # Check correct genotype
-        if lstr_maf_pos[ 2 ] == lstr_cur_vcf_entry[ 0 ] and lstr_maf_pos[ 3 ] == lstr_cur_vcf_entry[ 1 ]:
-          dict_gene_confirmed[ lstr_maf_pos[1] ] = dict_gene_confirmed.setdefault( lstr_maf_pos[1], 0 ) + 1
+        if lstr_maf_pos[ c_I_MAF_RETURN_REF ] == lstr_cur_vcf_entry[ c_I_VCF_RETURN_REF ] and lstr_maf_pos[ c_I_MAF_RETURN_GENOTYPE ] == lstr_cur_vcf_entry[ c_I_VCF_RETURN_GENOTYPE ]:
+          dict_gene_confirmed[ lstr_maf_pos[ c_I_MAF_RETURN_GENE_NAME ] ] = dict_gene_confirmed.setdefault( lstr_maf_pos[ c_I_MAF_RETURN_GENE_NAME ], 0 ) + 1
+          dict_gene_confirmed_sample.setdefault( lstr_maf_pos[ c_I_MAF_RETURN_GENE_NAME ], [] ).append( str_sample )
         else:
           # { Position : [ [ mafref maf/genotype ], [ vcfref vcf/genotype ] ] }
-          dict_wrong_genotype_pos[ lstr_maf_pos[ 0 ] ] = [ " ".join( [ lstr_maf_pos[ 2 ],lstr_maf_pos[ 3 ] ] ),
-                                                           " ".join( [ lstr_cur_vcf_entry[ 0 ],
-                                                           lstr_cur_vcf_entry[ 1 ] ] ), 
+          dict_wrong_genotype_pos[ lstr_maf_pos[ c_I_MAF_RETURN_POS ] ] = [ " ".join( [ lstr_maf_pos[ c_I_MAF_RETURN_REF ],lstr_maf_pos[ c_I_MAF_RETURN_GENOTYPE ] ] ),
+                                                           " ".join( [ lstr_cur_vcf_entry[ c_I_VCF_RETURN_REF ],
+                                                           lstr_cur_vcf_entry[ c_I_VCF_RETURN_GENOTYPE ] ] ), 
                                                            str_sample, 
-                                                           lstr_maf_pos[ 1 ] ]
+                                                           lstr_maf_pos[ c_I_MAF_RETURN_GENE_NAME ] ]
+          dict_gene_missed[ lstr_maf_pos[ c_I_MAF_RETURN_GENE_NAME ] ] = dict_gene_missed.setdefault( lstr_maf_pos[ c_I_MAF_RETURN_GENE_NAME ], 0 ) + 1
+          llstr_missed_detail.append( [ str_sample, dict_sample_mappings[ str_sample ], lstr_maf_pos[ c_I_MAF_RETURN_POS ], lstr_maf_pos[ c_I_MAF_RETURN_GENE_NAME ] ] )
+          dict_gene_missed_sample.setdefault( lstr_maf_pos[ c_I_MAF_RETURN_GENE_NAME ], [] ).append( str_sample )
       else:
-        dict_gene_missed[ lstr_maf_pos[1] ] = dict_gene_missed.setdefault( lstr_maf_pos[1], 0 ) + 1
-        llstr_missed_detail.append( [ str_sample, dict_sample_mappings[ str_sample ], lstr_maf_pos[0], lstr_maf_pos[1] ] )
+        dict_gene_missed[ lstr_maf_pos[ c_I_MAF_RETURN_GENE_NAME ] ] = dict_gene_missed.setdefault( lstr_maf_pos[ c_I_MAF_RETURN_GENE_NAME ], 0 ) + 1
+        llstr_missed_detail.append( [ str_sample, dict_sample_mappings[ str_sample ], lstr_maf_pos[ c_I_MAF_RETURN_POS ], lstr_maf_pos[ c_I_MAF_RETURN_GENE_NAME ] ] )
+        dict_gene_missed_sample.setdefault( lstr_maf_pos[ c_I_MAF_RETURN_GENE_NAME ], [] ).append( str_sample )
   else:
     lstr_samples_not_matched.append( str_sample )
 
@@ -179,33 +196,108 @@ if len( dict_wrong_genotype_pos ) > 0:
     lstr_cur_genotypes = dict_wrong_genotype_pos[ str_pos ]
     print "Sample: " + str_sample + " Position: " + str_pos + " Maf: " + lstr_cur_genotypes[ 0 ] + " VCF: " + lstr_cur_genotypes[ 1 ]
 
-# Plot as a combined bar plot
-lstr_write_genes = set( dict_gene_confirmed.keys() + dict_gene_missed.keys() )
-if lstr_key_genes:
-  lstr_write_genes = lstr_key_genes 
-li_genes_indicies = [ ( i_index * 3 ) + 1.5 for i_index in range( len( lstr_key_genes ) ) ]
-li_counts = []
-i_cur_count_index = 0
-li_counts_indices = []
-lstr_count_colors = []
-for str_gene in lstr_write_genes:
-  li_counts.append( dict_gene_confirmed.get( str_gene, 0 ) )
-  li_counts.append( dict_gene_missed.get( str_gene, 0 ) )
-  i_cur_count_index = i_cur_count_index + 1
-  li_counts_indices.append( i_cur_count_index )
-  i_cur_count_index = i_cur_count_index + 1
-  li_counts_indices.append( i_cur_count_index )
-  i_cur_count_index = i_cur_count_index + 1
-  lstr_count_colors.extend( [ "cyan", "magenta" ] )
 
+# Plot as a combined bar plot
+## All genes
+lstr_write_genes = set( dict_gene_confirmed.keys() + dict_gene_missed.keys() )
+## If key genes were given just look at key genes
+if lstr_key_genes:
+  lstr_write_genes = lstr_key_genes
+## Counts of exome and RNA-Seq genes
+li_counts = []
+## Used to help make indices
+i_cur_count_index = 0
+## Indices for counts (bars)
+li_counts_indices_exome = []
+li_counts_indices_rna = []
+## Count colors
+lstr_plot_genes = []
+li_exome_counts = []
+li_rna_counts = []
+## For each gene
+for str_gene in lstr_write_genes:
+  if dict_gene_confirmed.get( str_gene, 0 ) + dict_gene_missed.get( str_gene, 0 ):
+    # In RNA-Seq
+    li_rna_counts.append( dict_gene_confirmed.get( str_gene, 0 ) )
+    # In exome
+    li_exome_counts.append( dict_gene_confirmed.get( str_gene, 0 ) + dict_gene_missed.get( str_gene, 0 ) )
+    # Update positioning
+    i_cur_count_index = i_cur_count_index + 1
+    li_counts_indices_rna.append( i_cur_count_index )
+    i_cur_count_index = i_cur_count_index + 1
+    li_counts_indices_exome.append( i_cur_count_index )
+    i_cur_count_index = i_cur_count_index + 1
+    # Add gene name
+    lstr_plot_genes.append( str_gene )
+
+# Plot counts for genes
+## Positions for gene names
+li_genes_indicies = [ ( i_index * 3 ) + 1.5 for i_index in range( len( lstr_plot_genes ) ) ]
 fig, ax = plt.subplots()
-li_bar_indices = range( 1,len( lstr_write_genes ) + 3 )
-ax.barh( li_counts_indices, li_counts, color=lstr_count_colors )
-plt.yticks( li_genes_indicies, lstr_write_genes )
+ax.barh( li_counts_indices_rna, li_rna_counts, color=["cyan"]*len( li_counts_indices_rna), label="RNA-Seq" )
+ax.barh( li_counts_indices_exome, li_exome_counts, color=["magenta"]*len( li_counts_indices_exome ), label ="Exome"  )
+plt.yticks( li_genes_indicies, lstr_plot_genes )
 ax.set_title( "SNPs in Exome Seq Recovered in RNA-Seq" )
-ax.set_xlabel( "Count" )
+ax.set_xlabel( "SNP (Count)" )
 ax.set_ylabel( "Gene" )
 for tick in ax.yaxis.get_major_ticks():
   tick.label.set_fontsize(6)
+plt.legend( loc = 4 )
 plt.savefig( args_call.str_output_file )
 plt.close( fig )
+## Write data to file
+str_gene_text = os.path.splitext( args_call.str_output_file )[ 0 ] + ".txt"
+with open( str_gene_text, "w" ) as hndl_gene:
+  hndl_gene.write( "Gene\tExome\tRNA_Seq\n" )
+  for i_gene_index in xrange( len( lstr_plot_genes ) ):
+    hndl_gene.write( lstr_plot_genes[ i_gene_index ] + "\t" + str( li_exome_counts[ i_gene_index ] ) + "\t" + str( li_rna_counts[ i_gene_index ] ) + "\n" )
+
+# Plot counts for genes
+li_sample_counts_rna = []
+li_sample_counts_exome = []
+i_cur_count_index_sample = 0
+li_count_indices_sample_rna = []
+li_count_indices_sample_exome = []
+lstr_plot_genes_sample = []
+li_exome_counts_sample = []
+li_rna_counts_sample = []
+## For each gene
+for str_gene in lstr_write_genes:
+  if len( set( dict_gene_confirmed_sample.get( str_gene, [] ) + dict_gene_missed_sample.get( str_gene, [] ) ) ):
+    # In RNA-Seq
+    li_sample_counts_rna.append( len( set( dict_gene_confirmed_sample.get( str_gene, [] ) ) ) )
+    li_rna_counts_sample.append( len( set( dict_gene_confirmed_sample.get( str_gene, [] ) ) ) )
+    # In exome
+    li_sample_counts_exome.append( len( set( dict_gene_confirmed_sample.get( str_gene, [] ) + dict_gene_missed_sample.get( str_gene, [] ) ) ) )
+    li_exome_counts_sample.append( len( set( dict_gene_confirmed_sample.get( str_gene, [] ) + dict_gene_missed_sample.get( str_gene, [] ) ) ) )
+    # Update positioning
+    i_cur_count_index_sample = i_cur_count_index_sample + 1
+    li_count_indices_sample_rna.append( i_cur_count_index_sample )
+    i_cur_count_index_sample = i_cur_count_index_sample + 1
+    li_count_indices_sample_exome.append( i_cur_count_index_sample )
+    i_cur_count_index_sample = i_cur_count_index_sample + 1
+    # Add gene name
+    lstr_plot_genes_sample.append( str_gene )
+
+# Plot counts for genes
+## Positions for gene names
+str_sample_plot = os.path.splitext( args_call.str_output_file )[ 0 ] + "_samples.pdf"
+li_genes_indicies_samples = [ ( i_index * 3 ) + 1.5 for i_index in range( len( lstr_plot_genes_sample ) ) ]
+fig, ax = plt.subplots()
+ax.barh( li_count_indices_sample_rna, li_sample_counts_rna, color=["cyan"]*len(li_count_indices_sample_rna), label="RNA-Seq" )
+ax.barh( li_count_indices_sample_exome, li_sample_counts_exome, color=["magenta"]*len(li_count_indices_sample_exome ), label="Exome" )
+plt.yticks( li_genes_indicies_samples, lstr_plot_genes_sample )
+ax.set_title( "SNPs in Exome Seq Recovered in RNA-Seq" )
+ax.set_xlabel( "Sample (Count)" )
+ax.set_ylabel( "Gene" )
+for tick in ax.yaxis.get_major_ticks():
+  tick.label.set_fontsize(6)
+plt.legend( loc = 4 )
+plt.savefig( str_sample_plot )
+plt.close( fig )
+## Write data to file
+str_sample_text = os.path.splitext( str_sample_plot )[ 0 ] + ".txt"
+with open( str_sample_text, "w" ) as hndl_sample:
+  hndl_sample.write( "Gene\tExome_count\tRNA_Seq_count\n" )
+  for i_sample_index in xrange( len( lstr_plot_genes_sample ) ):
+    hndl_sample.write( lstr_plot_genes_sample[ i_sample_index ] + "\t" + str( li_exome_counts_sample[ i_sample_index ] ) + "\t" + str( li_rna_counts_sample[ i_sample_index ] ) + "\n" )
