7f397f4051ef154e43802fd39f238cd2031d2feb
Brian Haas <brianjohnhaas@gmail.com>
2015-06-02 13:57:32 -0400

    update

--- fusion/util/fusion-inspector-util/filter_FP_evidence_reads.pl
+++ fusion/util/fusion-inspector-util/filter_FP_evidence_reads.pl
@@ -10,6 +10,8 @@ use lib ("$FindBin::Bin/../../PerlLib");
 use Data::Dumper;
 use Pipeliner;
 use Fastq_reader;
+use SAM_reader;
+use SAM_entry;
 
 my $usage = <<__EOUSAGE__;
 
@@ -62,6 +64,9 @@ main: {
     my %spanning_frags;
     &parse_junction_and_spanning_reads($fusion_summary, \%junction_reads, \%spanning_frags);
 
+    print STDERR "-identified " . scalar(keys %junction_reads) . " junction reads.\n";
+    print STDERR "-identified " . scalar(keys %spanning_frags) . " spanning frags.\n";
+    
 
     {
         ##
@@ -80,9 +85,35 @@ main: {
             
             
             ## align the reads to the cdna fasta file
+            my $junc_reads_sam = "$junc_reads_fq.sam";
+            my $sam_chkpt = "$junc_reads_sam.ok";
+            my $bowtie_cmd = "bowtie -q -S --sam-nohead --best --chunkmbs 512 $cdna_fa  $junc_reads_fq > $junc_reads_sam";
+            &process_cmd($bowtie_cmd) unless (-e $sam_chkpt);
             
+            &process_cmd("touch $sam_chkpt");
             
+            ## eliminate those junction reads that do actually align ok to reference transcripts
+            my $number_reads_mapped = 0;
+            my $sam_reader = new SAM_reader($junc_reads_sam);
+            while (my $sam_entry = $sam_reader->get_next()) {
+                if (! $sam_entry->is_query_unmapped()) {
+                    my $full_read_name = $sam_entry->get_read_name();
+                    $number_reads_mapped++;
+                    
+                    if (delete $junction_reads{$full_read_name}) {
+                        print STDERR "-deleted junction read: $full_read_name\n";
+                    }
+                    else {
+                        print STDERR Dumper(\%junction_reads);
+                        die "Error, cannot delete junction read: $full_read_name\n";
+                    }
+                    
+                    
+                }
+            }
+            print STDERR "- $number_reads_mapped junction reads excluded due to mapping ok to reference transcripts\n";
             
+            #make checkpoint
             &process_cmd("touch $junc_reads_chkpt");
             
         }
@@ -103,8 +134,29 @@ main: {
 
             &extract_spanning_reads(\%spanning_frags, $left_fq, $span_reads_left_fq);
             &extract_spanning_reads(\%spanning_frags, $right_fq, $span_reads_right_fq);
-                        
+            
+            ## align the reads to the cdna fasta file
+            my $span_frags_sam = "tmp.span_reads.sam";
+            my $sam_chkpt = "$span_frags_sam.ok";
+            
+            my $bowtie_cmd = "bowtie -q -S --sam-nohead --best --chunkmbs 512 $cdna_fa -1 $span_reads_left_fq -2 $span_reads_right_fq > $span_frags_sam";
+            &process_cmd($bowtie_cmd) unless (-e $sam_chkpt);
 
+            ## eliminate those spanning frags that do actually align ok to reference transcripts
+            my $number_frags_mapped = 0;
+            my $sam_reader = new SAM_reader($span_frags_sam);
+            while (my $sam_entry = $sam_reader->get_next()) {
+                my $core_read_name = $sam_entry->get_core_read_name();
+                if ( (exists $spanning_frags{$core_read_name}) && $sam_entry->is_proper_pair() ) {
+                    
+                    $number_frags_mapped++;
+                    delete $spanning_frags{$core_read_name} or die "Error, could not delete $core_read_name from spanning_frags";
+                }
+            }
+            &process_cmd("touch $sam_chkpt");
+            
+            print STDERR "- $number_frags_mapped spanning frags excluded due to mapping ok to reference transcripts\n";
+            
             &process_cmd("touch $span_reads_chkpt");
         }
      
@@ -112,6 +164,17 @@ main: {
         
    
     }
+
+    
+    ##
+    ## report the adjusted fusion summary, removing the false evidence:
+    ##
+    
+    print STDERR "-post-filtering, have " . scalar(keys %junction_reads) . " junction reads.\n";
+    print STDERR "-post-filtering, have " . scalar(keys %spanning_frags) . " spanning frags.\n";
+    
+    &exclude_FP_junction_and_spanning_reads($fusion_summary, \%junction_reads, \%spanning_frags);
+    
 
     exit(0);
 
@@ -145,8 +208,10 @@ sub extract_junction_reads {
             my $full_read_name = $fq_obj->get_full_read_name();
             if (exists $junc_reads{$full_read_name}) {
                 my $fq_record = $fq_obj->get_fastq_record();
+                
                 print $ofh $fq_record;
                 delete $junc_reads{$full_read_name};
+                
             }
         }
     }
@@ -177,6 +242,10 @@ sub extract_spanning_reads {
     }
     close $ofh;
 
+    if (%frags) {
+        confess "Error, missing reads from file $fastq_input: " . Dumper(\%frags);
+    }
+    
     return;
 }
 
@@ -199,6 +268,47 @@ sub parse_junction_and_spanning_reads {
         foreach my $spanning_frag (split(/,/, $spanning_frag_list)) {
             $spanning_frags_href->{$spanning_frag}++;
         }
+    }
+    close $fh;
+
+    return;
+}
+
+
+####
+sub exclude_FP_junction_and_spanning_reads {
+    my ($fusion_summary, $junction_reads_href, $spanning_frags_href) = @_;
+
+    open (my $fh, $fusion_summary) or die "Error, cannot open file $fusion_summary";
+    while (<$fh>) {
+        if (/^\#/) { print; next; }
+        chomp;
+        my @x = split(/\t/);
+        my $junc_reads_list = $x[9];
+        my $spanning_frag_list = $x[10];
+
+        my @adj_junc_reads;
+        foreach my $junc_read (split(/,/, $junc_reads_list)) {
+            if (exists $junction_reads_href->{$junc_read}) {
+                push (@adj_junc_reads, $junc_read);
+            }
+        }
+
+        my @adj_spanning_frags;
+        foreach my $spanning_frag (split(/,/, $spanning_frag_list)) {
+            if (exists $spanning_frags_href->{$spanning_frag}) {
+                push (@adj_spanning_frags, $spanning_frag);
+            }
+        }
+
+        if (@adj_junc_reads) {
+            $x[9] = join(",", @adj_junc_reads);
+            $x[10] = join(",", @adj_spanning_frags);
+
+            print join("\t", @x) . "\n";
+        }
+        
+
     }
     close $fh;
 
