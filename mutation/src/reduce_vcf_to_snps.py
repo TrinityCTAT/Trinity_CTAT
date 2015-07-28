db4b0e7b5ba8d8710e76bac0e18cd9596a40c6a3
TimothyTickle <timothyltickle@gmail.com>
2015-07-28 16:33:23 -0400

    Made clearer messages.

--- mutation/src/reduce_vcf_to_snps.py
+++ mutation/src/reduce_vcf_to_snps.py
@@ -18,7 +18,7 @@ import csv
 
 prsr_arguments = argparse.ArgumentParser( prog = "reduce_vcf_to_snps.py", description = "Extracts only snp entries from a vcf file", formatter_class = argparse.ArgumentDefaultsHelpFormatter )
 prsr_arguments.add_argument( "--reference", action = "store_true", dest = "f_reference_mode", help = "Reference vcf file mode. Does not check to see if the vcf feature passes." )
-prsr_arguments.add_argument( "--no_prefilter", action="store_true", dest="f_no_prefilter_mode", help="When the VCF file has not been filtered (incidcated by using this flag), this script will not require the SNPs to pass a filter")
+prsr_arguments.add_argument( "--no_prefilter", action="store_true", dest="f_no_prefilter_mode", help="When the VCF file has not been filtered (indicated by using this flag), this script will not require the SNPs to pass a filter")
 prsr_arguments.add_argument( "str_input_file", help = "Input vcf file." )
 prsr_arguments.add_argument( "str_output_file", help = "Output SNP vcf file." )
 args = prsr_arguments.parse_args()
@@ -56,7 +56,7 @@ if args.str_input_file:
         # Make sure the variant passes.
         elif not args.f_no_prefilter_mode:
           if not lstr_line[ I_FILTER_INDEX ].lower() == STR_PASS.lower():
-            print "Did not pass filter."
+            print "Did not pass filter. "+lstr_line[ I_FILTER_INDEX ].lower()+"."
             continue
 
         # Get ALT / REF
@@ -74,10 +74,10 @@ if args.str_input_file:
         # Not removing the none snp information currently because the DBSNP info would have to be updated to reflect any indexing
         # referring to the REF or ALT
         if not min( [ len( str_alt_token ) for str_alt_token in str_alt.split( "," ) ] ) == 1:
-          print "skip not snp 1"
+          print "Skip not snp ALT"
           continue
         if not min( [ len( str_ref_token ) for str_ref_token in str_ref.split( "," ) ] ) == 1:
-          print "skip not snp 2"
+          print "Skip not snp REF"
           continue
    
         # Store SNP
