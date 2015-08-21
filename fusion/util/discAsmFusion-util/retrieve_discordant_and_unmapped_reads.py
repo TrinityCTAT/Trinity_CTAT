c5474a4f246b9ab5ff241c85c81c843913aa6e35
Brian Haas <brianjohnhaas@gmail.com>
2015-08-21 18:04:52 -0400

    including DiscordAFuse

--- fusion/util/discAsmFusion-util/retrieve_discordant_and_unmapped_reads.py
+++ fusion/util/discAsmFusion-util/retrieve_discordant_and_unmapped_reads.py
@@ -27,10 +27,13 @@ def main():
         if read.is_proper_pair:
             proper_pairs.add(read.query_name)
 
+    
+    left_fq_extracted_filename = left_fq_filename + ".extracted.fq"
+    right_fq_extracted_filename = right_fq_filename + ".extracted.fq"
 
-    left_fq_extraction_thread = Unkept_read_fq_extractor(proper_pairs, left_fq_filename, left_fq_filename + ".extracted.fq")
-    right_fq_extraction_thread = Unkept_read_fq_extractor(proper_pairs, right_fq_filename, right_fq_filename + ".extracted.fq")
-
+    left_fq_extraction_thread = Unkept_read_fq_extractor(proper_pairs, left_fq_filename, left_fq_extracted_filename)
+    right_fq_extraction_thread = Unkept_read_fq_extractor(proper_pairs, right_fq_filename, right_fq_extracted_filename)
+    
     left_fq_extraction_thread.start()
     right_fq_extraction_thread.start()
 
@@ -40,6 +43,8 @@ def main():
         if not t.success:
             num_failed += 1
             print >>sys.stderr, "Error extracting reads from file: " + t.input_fq_filename
+
+
 
     sys.exit(num_failed)
     
@@ -67,8 +72,16 @@ class Unkept_read_fq_extractor (threading.Thread):
     def run(self):
         ## do the extraction
 
+        ofh = open(self.output_fq_filename, 'w')
         
-
+        fq_reader = pysam.FastqFile(self.input_fq_filename)
+        for fq_entry in fq_reader:
+            read_name = fq_entry.name
+            read_name = re.sub("/[12]$", "", read_name)
+            if read_name not in self.keep_set:
+                ofh.write( "\n".join([fq_entry.name, fq_entry.quality, "+", fq_entry.sequence]) + "\n")
+        
+        ofh.close()
 
         self.success = True
 
