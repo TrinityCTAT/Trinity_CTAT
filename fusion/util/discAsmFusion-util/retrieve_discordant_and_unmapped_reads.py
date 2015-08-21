4306dc41d53c0a7eb9a36fc892a2a3e2981714d8
Brian Haas <brianjohnhaas@gmail.com>
2015-08-21 19:10:24 -0400

    proper fq output now

--- fusion/util/discAsmFusion-util/retrieve_discordant_and_unmapped_reads.py
+++ fusion/util/discAsmFusion-util/retrieve_discordant_and_unmapped_reads.py
@@ -79,7 +79,11 @@ class Unkept_read_fq_extractor (threading.Thread):
             read_name = fq_entry.name
             read_name = re.sub("/[12]$", "", read_name)
             if read_name not in self.keep_set:
-                ofh.write( "\n".join([fq_entry.name, fq_entry.quality, "+", fq_entry.sequence]) + "\n")
+                ofh.write( "\n".join(["@" + fq_entry.name,
+                                      fq_entry.quality,
+                                      "+",
+                                      fq_entry.sequence]
+                                     ) + "\n")
         
         ofh.close()
 
