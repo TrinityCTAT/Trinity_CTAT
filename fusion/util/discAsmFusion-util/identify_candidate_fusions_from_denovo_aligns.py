82ef7db717b386d2a8151d9d4400c4d7d8867b14
Brian Haas <brianjohnhaas@gmail.com>
2015-08-23 22:28:44 -0400

    works on minigene gff3 now

--- fusion/util/discAsmFusion-util/identify_candidate_fusions_from_denovo_aligns.py
+++ fusion/util/discAsmFusion-util/identify_candidate_fusions_from_denovo_aligns.py
@@ -2,27 +2,18 @@
 
 import sys, os, re
 
-usage = "\n\n\tusage: " + sys.argv[0] + " chimera.trans.align.gff3 cdna.fasta\n\n"
+usage = "\n\n\tusage: " + sys.argv[0] + " chimera.trans.align.gff3\n\n"
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 2:
     print >>sys.stderr, usage
     sys.exit(1)
 
 chimera_trans_align_gff3 = sys.argv[1]
-cdna_fasta_filename = sys.argv[2]
-
 
 
 def main():
     
-    headers_file = cdna_fasta_filename + ".headers"
-    if not os.path.exists(headers_file):
-        print >>sys.stderr, "Error, cannot locate headers file: " + headers_file
-        sys.exit(2)
-
-    trans_to_gene_hash = parse_trans_to_gene_from_header(headers_file)
-
-    trans_to_gene_pairs = parse_trans_to_gene_pairs(chimera_trans_align_gff3, trans_to_gene_hash)
+    trans_to_gene_pairs = parse_trans_to_gene_pairs(chimera_trans_align_gff3)
 
     for trans_acc in trans_to_gene_pairs:
         gene_hits_array = trans_to_gene_pairs[trans_acc]
@@ -34,9 +25,9 @@ def main():
         gene_2_name = gene_hits_array[1][0]
         gene_2_orient = gene_hits_array[1][1]
 
-        print "\t".join(str(x) for x in [trans_acc, gene_1_name, gene_1_orient, gene_2_name, gene_2_orient])
+        #print "\t".join(str(x) for x in [trans_acc, gene_1_name, gene_1_orient, gene_2_name, gene_2_orient]); continue
 
-        continue
+        if gene_1_name == gene_2_name: continue
         
         if gene_1_orient != gene_2_orient: continue
 
@@ -53,7 +44,7 @@ def main():
 
 
 
-def parse_trans_to_gene_pairs(chimera_gff3, trans_to_gene_hash):
+def parse_trans_to_gene_pairs(chimera_gff3):
 
     trans_to_gene_pairs = {}
 
@@ -65,7 +56,11 @@ def parse_trans_to_gene_pairs(chimera_gff3, trans_to_gene_hash):
         x = line.split("\t")
 
         info = x[8]
-        trans_acc = x[0]
+        gene_contig_acc = x[0]
+
+        gene_name = gene_contig_acc.replace("__", " ").rstrip().split(" ")[-1]
+        
+
         orient = x[6]
         
         path_num = re.search(".path(\d);", info).group(1)
@@ -73,8 +68,7 @@ def parse_trans_to_gene_pairs(chimera_gff3, trans_to_gene_hash):
         path_num -= 1
 
         denovo_trans_id = re.search("Target=(\S+)", info).group(1)
-
-        gene_name = trans_to_gene_hash[trans_acc]
+        
 
         if denovo_trans_id not in trans_to_gene_pairs:
             trans_to_gene_pairs[denovo_trans_id] = [ [None,None], [None,None] ]
