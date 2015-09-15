92025db1f17c7403c46a68ab24d49ee7c30ab5d4
TimothyTickle <timothyltickle@gmail.com>
2015-09-15 11:18:06 -0400

    Added authorship info.

--- mutation/src/annotate_with_cravat.py
+++ mutation/src/annotate_with_cravat.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+__author__ = "Timothy Tickle"
+__copyright__ = "Copyright 2015"
+__credits__ = [ "Timothy Tickle", "Brian Haas" ]
+__license__ = "MIT"
+__maintainer__ = "Timothy Tickle"
+__email__ = "ttickle@broadinstitute.org"
+__status__ = "Development"
+
 import argparse
 import json
 import os
