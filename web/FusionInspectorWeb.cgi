82dec11eb26b7bad72ff87e1c31a26b02cc7f3cd
TimothyTickle <timothyltickle@gmail.com>
2015-10-13 08:58:02 -0400

    Added fusion web elements.

---
+++ web/FusionInspectorWeb.cgi
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+print( "Content-Type: text/html\n\n" )
+print( """<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="description" content="Trintiy CRTAT visualization for SNVs in discovered RNA-Seq.">
+    <meta name="author" content="Brian Haas,Timothy Tickle">
+    <title>Trinity CTAT Fusion Inspector</title>
+
+    <!-- CSS -->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <!-- jQuery UI CSS -->
+    <link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/themes/smoothness/jquery-ui.css"/>
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+    <!-- IGV CSS -->
+    <link rel="stylesheet" type="text/css" href="//igv.org/web/beta/igv.css">
+    <!-- inspector css -->
+    <link rel="stylesheet" type="text/css" href="css/inspector.css">
+
+    <!-- Scripts -->
+    <!-- jQuery JS -->
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <!-- IGV JS-->
+    <script type="text/javascript" src="//igv.org/web/beta/igv-all.min.js"></script>
+</head>    
+<body>
+    <!-- Header (info) -->
+    <div class-"constainer-fluid" id="sampleHeader">
+        <div class="row" style="background-color: #E7E7EF">
+            <div class="col-xs-6"><p>Sample:</p></div>
+            <div class="col=xs-6">
+                <p>Fusion</p>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <!--- End header )info) -->
+
+    <!-- IGV browser area -->
+    <div id="igvBrowser"></div>
+
+    <!-- Scripts -->
+    <script src='fusion_inspector.json'></script>
+    <script src='js/FusionInspector.js'></script>
+    <script>
+    var igvBrowser = loadFusionIGV( fusionInspector )
+    </script>
+</body>
+</html> """ )
---
+++ web/js/FusionInspector.js
@@ -0,0 +1,75 @@
+function loadFusionIGV( sampleInfo )
+{
+    var div = $("#igvBrowser")[0],
+    options = {
+        showKaryo: false,
+        showNavigation: true,
+        fastaURL: sampleInfo.reference,
+        cytobandURL: sampleInfo.cytoband,
+        tracks: [{
+                     type: "sequence",
+                     order: 1
+                 },
+                 {
+                     label: "Reference_Annotations",
+                     url: sampleInfo.referenceBed,
+                     displayMode: "SQUISHED",
+                     indexed: false,
+                     order: 10
+                 },
+                 {
+                     label: "Junction_Spanning_View",
+                     type: "FusionJuncSpan",
+                     url: sampleInfo.junctionSpanning,
+                     displayMode: "SQUISHED",
+                     indexed: false,
+                     order: 20
+                 },
+                 {
+                     label: "Trinity_Fusion",
+                     url: sampleInfo.trinityBed,
+                     displayMode: "EXPANDED",
+                     color: "green",
+                     indexed: false,
+                     order: 30
+                 },
+                 {
+                     url: sampleInfo.junctionReads,
+                     label: "Junction_Reads",
+                     displayMode: "SQUISHED",
+                     minHeight: 30,
+                     maxHeight: 100,
+                     color: "cyan",
+                     indexed: false,
+                     order: 40
+                 },
+                 {
+                     url: sampleInfo.junctionReadsBam,
+                     indexURL: sampleInfo.junctionReadsBai,
+                     label: "Junction_Reads_Alignments",
+                     height: 100,
+                     indexed: true,
+                     visibilityWindow: 2000000,
+                     order: 45
+                 },
+                 {
+                     url: sampleInfo.spanningReads,
+                     label: "Spanning_Reads",
+                     displayMode: "SQUISHED",
+                     minHeight: 30,
+                     maxHeight: 100,
+                     color: "purple",
+                     indexed: false,
+                     order: 50
+                 },
+                 {
+                     url: sampleInfo.spanningReadsBam,
+                     indexURL: sampleInfo.spanningReadsBai,
+                     label: "Spanning_Reads_Alignments",
+                     height: 100,
+                     indexed: true,
+                     visibilityWindow: 2000000,
+                     order: 55
+                 }]};
+    igv.createBrowser(div, options);
+}
