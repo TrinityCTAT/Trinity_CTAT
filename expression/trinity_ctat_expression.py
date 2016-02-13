59d7d925c99430f8de4f566f77dd4711300be0d8
kcopipe <kcopipe@delanatii.broadinstitute.org>
2016-02-13 15:21:41 -0500

    no environ paths

--- expression/trinity_ctat_expression.py
+++ expression/trinity_ctat_expression.py
@@ -9,15 +9,12 @@ __maintainer__ = "Timothy Tickle"
 __email__ = "bankapur@broadinstitute.org"
 __status__ = "Development"
 
-#import inspect
 import os
 import sciedpiper.Command as Command
 import sciedpiper.ParentScript as ParentScript
 
-##tools_home = os.environ['TOOLS']
-##kallisto_prg = os.path.realpath( os.path.join(tools_home,'external_libs','kallisto') )
-KALLISTO = 'KALLISTO'
-
+KALLISTO = 'kallisto'
+kallisto_script = 'kallisto_script'
 
 class ExpressionScript( ParentScript.ParentScript ):
     
@@ -34,7 +31,7 @@ class ExpressionScript( ParentScript.ParentScript ):
 
         arg_raw.description = "Expression from RNASeq Data for Trinity CTAT"
         arg_raw.add_argument( "--left_fq", required=True, help = "Left read fastq" )        
-        arg_raw.add_argument( "--right_fq", required=False ,default="", help ="Right read fastq" )
+        arg_raw.add_argument( "--right_fq", default="", help ="Right read fastq" )
         arg_raw.add_argument( "--annot_config", required=True ,help ="Annotation Config file" )
         arg_raw.add_argument( "--bias", action="store_true", help= "Perform sequence based bias correction")
         arg_raw.add_argument( "--bootstrap_samples", help ="Number of bootstrap samples" )
@@ -47,10 +44,10 @@ class ExpressionScript( ParentScript.ParentScript ):
         # Make directories and check files that need to exist before beginning
  
         kallisto_dir = os.path.join( args_parsed.str_file_base, KALLISTO )
-        cur_pipeline.func_mkdirs( [ KALLISTO ] )
+        cur_pipeline.func_mkdirs( [ kallisto_dir  ] )
 
         ### Get kallisto script command
-        kallisto_cmd = [ KALLISTO + ' quant ' ] 
+        kallisto_cmd = [ kallisto_script + ' quant' ] 
         
         if args_parsed.annot_config is not None:
            with open( args_parsed.annot_config, 'r' ) as annot:
@@ -65,22 +62,22 @@ class ExpressionScript( ParentScript.ParentScript ):
            option_cmd2 = [ " --bias " ]
            kallisto_cmd.extend( option_cmd2 )
 
-        if args_parsed.bootstrap_samples != "" :
+        if not args_parsed.bootstrap_samples:
            option_cmd3 = [ " --bootstrap-samples " + args_parsed.bootstrap_samples ]
            kallisto_cmd.extend( option_cmd3 )
 
-        if args_parsed.threads != "":
+        if not args_parsed.threads:
            option_cmd4 = [ " --threads " + args_parsed.threads ]
            kallisto_cmd.extend( option_cmd4 )
 
-        if args_parsed.seed != "":
+        if not args_parsed.seed:
            option_cmd5 = [ " --seed  " + args_parsed.seed ]
            kallisto_cmd.extend( option_cmd5 )
         
         option_cmd6 = [ " -o " + kallisto_dir ]
         kallisto_cmd.extend( option_cmd6 )
 
-        if args_parsed.right_fq == "None":
+        if not args_parsed.right_fq:
            option_cmd7 = [ " --single " + args_parsed.left_fq ] 
         else:
            option_cmd7 = [ args_parsed.left_fq + " " + args_parsed.right_fq ]
