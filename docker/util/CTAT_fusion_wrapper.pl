3790b9c64179e9bad13605d03414f815ed86d587
Brian Haas <brianjohnhaas@gmail.com>
2016-03-21 10:44:58 -0400

    update

--- docker/util/CTAT_fusion_wrapper.pl
+++ docker/util/CTAT_fusion_wrapper.pl
@@ -15,24 +15,23 @@ my $STAR_FUSION_HOME = $ENV{STAR_FUSION_HOME} or die "Error, env var STAR_FUSION
 main: {
 
     my $cmd = "tar xvf $ctat_genome_lib_tar_gz";
-    &process_cmd($cmd);
+    &process_cmd($cmd) unless (-d "CTAT_lib");
 
-    $cmd = "/usr/local/src/STAR-Fusion_v0.7.0_FULL/STAR-Fusion " .
+    $cmd = "$STAR_FUSION_HOME/STAR-Fusion " .
 	" --left_fq  $left_fq_gz" .
 	" --right_fq $right_fq_gz" . 
         " --genome_lib_dir CTAT_lib" .
 	" --output_dir star_fusion_outdir";
 
-    &process_cmd($cmd);
+    &process_cmd($cmd) unless (-d "star_fusion_outdir");
     
     $cmd = "tar -zcvf star_fusion_outdir.tar.gz star_fusion_outdir";
-    &process_cmd($cmd);
-
+    &process_cmd($cmd) unless (-s "star_fusion_outdir.tar.gz");
+    
     print STDERR "Done.\n";
     
     exit(0);
     
-
 }
 
 ####
