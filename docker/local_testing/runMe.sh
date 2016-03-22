f54583d9b482948f1d1cb342809f0d409b794bdf
Brian Haas <brianjohnhaas@gmail.com>
2016-03-22 10:08:12 -0400

    update

--- docker/local_testing/runMe.sh
+++ docker/local_testing/runMe.sh
@@ -10,10 +10,10 @@ utildir=`dirname $0`/..
 
 export PERL5LIB=${utildir}/PerlLib:${PERL5LIB}
 
-cmd=${utildir}/util/CTAT_fusion_wrapper.pl /seq/RNASEQ/CANCER_FUSION_RESOURCES/BT-474/BT474.Left.fq.gz \
+cmd="${utildir}/util/CTAT_fusion_wrapper.pl /seq/RNASEQ/CANCER_FUSION_RESOURCES/BT-474/BT474.Left.fq.gz \
                                            /seq/RNASEQ/CANCER_FUSION_RESOURCES/BT-474/BT474.Right.fq.gz \
                                            /seq/RNASEQ/public_ftp/CTAT_lib.tar.gz \
-                                           DISCASM
+                                           DISCASM"
 
 eval $cmd
 
