93b4b410bc78ae7dc273aecfd30317352a295771
TimothyTickle <timothyltickle@gmail.com>
2016-05-10 09:53:21 -0400

    Formating.

--- mutation/src/filter_vcf_for_cancer.py
+++ mutation/src/filter_vcf_for_cancer.py
@@ -20,9 +20,13 @@ STR_FATHMM = "FATHMM"
 STR_FATHMM_NEUTRAL = "PASSENGER/OTHER"
 
 # Arguments
-prsr_arguments = argparse.ArgumentParser( prog = "filter_vcf_for_cancer.py", description = "Extracts common variants which do not have COSMIC ids entries from a vcf file", formatter_class = argparse.ArgumentDefaultsHelpFormatter )
-prsr_arguments.add_argument( "str_input_file", help = "Input vcf.gz file." )
-prsr_arguments.add_argument( "str_output_file", help = "Output filtered vcf file." )
+prog_desc = "".join(["Extracts common variants which do",
+                     "not have COSMIC ids entries from a vcf file"])
+prsr_arguments = argparse.ArgumentParser(prog="filter_vcf_for_cancer.py",
+                                         description=prog_desc,
+                                         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+prsr_arguments.add_argument("str_input_file", help="Input vcf.gz file.")
+prsr_arguments.add_argument("str_output_file", help="Output filtered vcf file.")
 args = prsr_arguments.parse_args()
 
 # Stores the vcf info
@@ -35,7 +39,7 @@ i_depth = 0
 i_origin = 0
 i_fathmm = 0
 
-def func_split_token( str_token ):
+def func_split_token(str_token):
   """
   Splits a VCF info token while guarenteeing 2 derived pieces.
   * str_token : String token to split at '='
@@ -45,54 +49,58 @@ def func_split_token( str_token ):
   """
   if str_token:
     lstr_pieces = str_token.split("=")
-    i_pieces = len( lstr_pieces )
+    i_pieces = len(lstr_pieces)
     if i_pieces == 1:
-      return( lstr_pieces + [ "" ] )
+      return(lstr_pieces + [""])
     if i_pieces == 2:
       return lstr_pieces
     elif i_pieces > 2:
-      return [ lstr_pieces[ 0 ], "=".join( lstr_pieces[1:] ) ]
-  return [ "", "" ]
+      return [lstr_pieces[0], "=".join(lstr_pieces[1:])]
+  return ["", ""]
 
 # Get handle
-str_input_ext = os.path.splitext( args.str_input_file )[1]
-hndl_vcf = gzip.open( args.str_input_file, "rb" ) if str_input_ext == ".gz" else open( args.str_input_file, "rb" )
+str_input_ext = os.path.splitext(args.str_input_file)[1]
+hndl_vcf = gzip.open(args.str_input_file, "rb") if str_input_ext == ".gz" else open(args.str_input_file, "rb")
 
 # Read in vcf file
 if args.str_input_file:
-  with open( args.str_output_file, "w" ) as hndl_out:
-    for lstr_line in csv.reader( hndl_vcf, delimiter = STR_VCF_DELIMITER ):
+  with open(args.str_output_file, "w") as hndl_out:
+    for lstr_line in csv.reader(hndl_vcf, delimiter = STR_VCF_DELIMITER):
 
       # Keep comments
       if lstr_line[0][0] == CHR_COMMENT:
-        lstr_vcf.append( STR_VCF_DELIMITER.join( lstr_line ) )
+        lstr_vcf.append(STR_VCF_DELIMITER.join(lstr_line))
         continue
 
       # Parse INFO tokens
-      dict_info_tokens = dict( [ func_split_token( str_token ) for str_token in lstr_line[ I_INFO_INDEX ].split( CHR_INFO_DELIMITER ) ] )
-      
+      dict_info_tokens = dict([func_split_token(str_token) for str_token in lstr_line[I_INFO_INDEX].split(CHR_INFO_DELIMITER)])
+
       # Filtering
       ## Filter out common variant that do not have cosmic ids
       if STR_COMMON_VARIANT in dict_info_tokens:
-        if ( dict_info_tokens[ STR_COMMON_VARIANT ] == "1" ) and ( STR_COSMIC_ID not in dict_info_tokens ):
+        if((dict_info_tokens[STR_COMMON_VARIANT] == "1")
+           and (STR_COSMIC_ID not in dict_info_tokens)):
           i_common = i_common + 1
           continue
 
       ## Filter DP < 10
       if STR_DEPTH in dict_info_tokens:
-        if ( int( dict_info_tokens[ STR_DEPTH ] ) < 10 ) and ( STR_COSMIC_ID not in dict_info_tokens ):
+        if((int(dict_info_tokens[STR_DEPTH]) < 10)
+           and (STR_COSMIC_ID not in dict_info_tokens)):
           i_depth = i_depth + 1
           continue
 
       ## SAO = Germline
       if STR_ORIGIN in dict_info_tokens:
-        if ( dict_info_tokens[ STR_ORIGIN ] == STR_ORIGIN_GERMLINE ) and ( STR_COSMIC_ID not in dict_info_tokens ):
+        if((dict_info_tokens[STR_ORIGIN] == STR_ORIGIN_GERMLINE)
+           and (STR_COSMIC_ID not in dict_info_tokens)):
           i_origin = i_origin + 1
           continue
 
       ## FATHMM = Neutral or passenger
       if STR_FATHMM in dict_info_tokens:
-        if ( dict_info_tokens[ STR_FATHMM ] == STR_FATHMM_NEUTRAL ) and ( STR_COSMIC_ID not in dict_info_tokens ):
+        if((dict_info_tokens[STR_FATHMM] == STR_FATHMM_NEUTRAL)
+           and (STR_COSMIC_ID not in dict_info_tokens)):
           i_fathmm = i_fathmm + 1
           continue
 
@@ -102,29 +110,32 @@ if args.str_input_file:
       ## U5
 
       # TODO could add
-      # SSR,Number=1,Type=Integer,Description="Variant Suspect Reason Codes (may be more than one value added together) 0 - unspecified, 1 - Paralog, 2 - byEST, 4 - oldAlign, 8 - Para_EST, 16 - 1kg_failed, 1024 - other"
+      # SSR,Number=1,Type=Integer,
+      # Description="Variant Suspect Reason Codes (may be more than one
+      # value added together) 0 - unspecified, 1 - Paralog, 2 - byEST,
+      # 4 - oldAlign, 8 - Para_EST, 16 - 1kg_failed, 1024 - other"
       # SSR
 
       # Store passing variant
-      lstr_vcf.append( STR_VCF_DELIMITER.join( lstr_line ) )
+      lstr_vcf.append(STR_VCF_DELIMITER.join(lstr_line))
 
       # If buffer is large enough, write to file
-      if len( lstr_vcf ) >= i_write_amount:
-        str_write = "\n".join( lstr_vcf )
+      if len(lstr_vcf) >= i_write_amount:
+        str_write = "\n".join(lstr_vcf)
         if str_write:
           str_write = str_write + "\n"
-        hndl_out.write( str_write )
+        hndl_out.write(str_write)
         lstr_vcf = []
 
     # Last write of buffer
-    hndl_out.write( "\n".join( lstr_vcf ) )
+    hndl_out.write("\n".join(lstr_vcf))
 
 # Close input handle
 hndl_vcf.close()
 
 print "Filtering by class:"
 print "(Features in multiple classes are counted in the first to filter them)"
-print "Common: " + str( i_common )
-print "Depth: " + str( i_depth )
-print "Origin: " + str( i_origin )
-print "FATHMM: " + str( i_fathmm )
+print "Common: " + str(i_common)
+print "Depth: " + str(i_depth)
+print "Origin: " + str(i_origin)
+print "FATHMM: " + str(i_fathmm)
