4bff07bb69f6f2cef270e7b97e707722d923cb67
brianjohnhaas <brianjohnhaas@gmail.com>
2017-08-05 13:53:44 +0000

    extra read cleaning

--- FireCloud/CTAT_fusion/docker/util/sam_readname_cleaner.py
+++ FireCloud/CTAT_fusion/docker/util/sam_readname_cleaner.py
@@ -47,9 +47,32 @@ def clean_reads(input_bam, output_bam):
     sam_reader = subprocess.Popen(["samtools", "view", "-h", input_bam], stdout=subprocess.PIPE)
     sam_writer = subprocess.Popen(["samtools", "view", "-Sb", "-o", output_bam], stdin=subprocess.PIPE)
 
+        
     for line in sam_reader.stdout:
+        if re.search("^@", line):
+            # header line
+            sam_writer.stdin.write(line)
+            continue
+        
+        # adjust sam record
         x = line.split("\t")
+        m = re.search("/([12])$", x[0])
+        if not m:
+            sys.stderr.write("Error, cannot discern /1 or /2 from read name: [{}] ".format(x[0]))
+            sys.exit(1)
+            
+        read_dir = m.group(1)
         x[0] = re.sub("/[12]$", "", x[0])
+        flag = int(x[1])
+
+        flag = flag | 1  # paired read
+        if read_dir == "1":
+            flag = flag | 64
+        else:
+            flag = flag | 128
+
+        x[1] = str(flag)
+        
         sam_writer.stdin.write("\t".join(x))
     
 
